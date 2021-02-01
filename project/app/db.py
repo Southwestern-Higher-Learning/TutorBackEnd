b30e2206eaf4d11a03fe37c2f3969f5f57c4a881
Jake Gutierrez <jakegut0108@gmail.com>
2021-02-01 16:28:08 -0600

    Fix formatting

--- project/app/db.py
+++ project/app/db.py
@@ -2,9 +2,8 @@ import logging
 import os
 
 from fastapi import FastAPI
-from tortoise import Tortoise, run_async # updated
+from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
-
 
 log = logging.getLogger("uvicorn")
 
@@ -35,13 +34,12 @@ async def generate_test_schema() -> None:
 
     await Tortoise.init(
         db_url=os.environ.get("DATABASE_TEST_URL"),
-        modules={'models': ['models.tortoise']},
+        modules={"models": ["models.tortoise"]},
     )
     log.info("Generating database schema via Tortoise...")
     await Tortoise.generate_schemas()
     await Tortoise.close_connections()
 
 
-# new
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_async(generate_test_schema())
