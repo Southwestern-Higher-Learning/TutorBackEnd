6263379ef6db4758e947b9a0584e73494052c17d
Jake Gutierrez <jakegut0108@gmail.com>
2021-02-04 21:26:33 -0600

    Try again

--- project/app/db.py
+++ project/app/db.py
@@ -32,7 +32,7 @@ async def generate_test_schema() -> None:
     log.info("Initializing Tortoise...")
 
     await Tortoise.init(
-        db_url=os.environ.get("DATABASE_URL"),
+        db_url=os.environ.get("DATABASE_TEST_URL"),
         modules={"models": ["models.tortoise"]},
     )
     log.info("Generating database schema via Tortoise...")
