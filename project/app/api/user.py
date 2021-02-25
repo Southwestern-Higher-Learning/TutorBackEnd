a3d70bea69cc797cb783e89e9b4a3393d3d3ede8
Jake Gutierrez <jakegut0108@gmail.com>
2021-02-25 10:50:50 -0600

    get rid of operation_ids

--- project/app/api/user.py
+++ project/app/api/user.py
@@ -39,13 +39,13 @@ async def find_current_superuser(
 
 
 # GET /user/me find the current user based on the access_token
-@router.get("/me", response_model=User_Pydnatic, operation_id="authorize")
+@router.get("/me", response_model=User_Pydnatic)
 async def get_current_user(current_user: User = Depends(find_current_user)):
     return await User_Pydnatic.from_tortoise_orm(current_user)
 
 
 # PATCH /user/me update user profile based on current user
-@router.patch("/me", response_model=User_Pydnatic, operation_id="authorize")
+@router.patch("/me", response_model=User_Pydnatic)
 async def patch_current_user(
     user_update: NormalUserUpdate, current_user: User = Depends(find_current_user)
 ):
@@ -54,7 +54,7 @@ async def patch_current_user(
 
 
 # GET /user/{user_id}
-@router.get("/{user_id}", response_model=User_Pydnatic, operation_id="authorize")
+@router.get("/{user_id}", response_model=User_Pydnatic)
 async def get_user_id(user_id: int, current_user: User = Depends(find_current_user)):
     return await User_Pydnatic.from_queryset_single(User.get(id=user_id))
 
