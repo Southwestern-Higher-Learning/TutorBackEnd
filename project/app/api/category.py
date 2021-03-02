994119e91441136bf8417caf08a2b990e5219ddb
blead00 <70769383+blead00@users.noreply.github.com>
2021-03-02 08:59:53 -0600

    Changed the response model for the get_categories route to a List

--- project/app/api/category.py
+++ project/app/api/category.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 
 from app.api.user import find_current_user
@@ -9,7 +11,7 @@ router = APIRouter(prefix="/category", tags=["category"])
 # GET /
 # Must be normal user (find_current_user)
 # Get all categories
-@router.get("/", response_model=Category_Pydnatic)
+@router.get("/", response_model=List[Category_Pydnatic])
 async def get_categories(current_user: User = Depends(find_current_user)):
     return await Category_Pydnatic.from_queryset(Category.all())
 
@@ -20,6 +22,7 @@ async def get_categories(current_user: User = Depends(find_current_user)):
 # POST /
 # Must be superuser (find_current_superuser)
 # Create new category
+
 
 # PUT /{id}
 # Must be superuser
