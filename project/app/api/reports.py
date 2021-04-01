66f252bd43a222ee9bcc47afd1c459bd691f3729
greathod <46719991+greathod@users.noreply.github.com>
2021-04-01 09:19:52 -0500

    Adjusted delete route in reports.py
    Adjusted the JSONResponse in the delete route in reports.py.

--- project/app/api/reports.py
+++ project/app/api/reports.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi.responses import JSONResponse
 
 from app.api.user import find_current_superuser, find_current_user
 from app.models.pydnatic import ReportFilters
@@ -18,13 +19,13 @@ pageinate_report = PaginateModel(Report, ReportFilters)
 # try to use the pagination thing like in user and categories
 @router.get("/", response_model=List[Report_Pydnatic])
 async def get_reports(
-        response: Response,
-        current_superuser: User = Depends(find_current_superuser),
-        reports=Depends(pageinate_report),
+    response: Response,
+    current_superuser: User = Depends(find_current_superuser),
+    reports=Depends(pageinate_report),
 ):
     len_reports = await reports.count()
     response.headers["X-Total-Count"] = f"{len_reports}"
-    return await Report_Pydnatic.from_queryset(categories)
+    return await Report_Pydnatic.from_queryset(reports)
 
 
 # GET /
@@ -32,8 +33,7 @@ async def get_reports(
 # Get report by id
 @router.get("/{report_id}", response_model=Report_Pydnatic)
 async def get_report_id(
-    report_id: int,
-    current_superuser: User = Depends(find_current_superuser)
+    report_id: int, current_superuser: User = Depends(find_current_superuser)
 ):
     return await Report_Pydnatic.from_queryset_single(Report.get(id=report_id))
 
@@ -43,8 +43,7 @@ async def get_report_id(
 # Create a new report
 @router.post("/", response_model=Report_Pydnatic)
 async def create_report(
-        report: ReportIn_Pydnatic,
-        current_user: User = Depends(find_current_user)
+    report: ReportIn_Pydnatic, current_user: User = Depends(find_current_user)
 ):
     report_obj = await Report.create(**report.dict(exclude_unset=True))
     return await Report_Pydnatic.from_tortoise_orm(report_obj)
@@ -55,9 +54,9 @@ async def create_report(
 # update a report
 @router.put("/", response_model=Report_Pydnatic)
 async def update_report(
-        report: ReportIn_Pydnatic,
-        report_id: int,
-        current_superuser: User = Depends(find_current_superuser),
+    report: ReportIn_Pydnatic,
+    report_id: int,
+    current_superuser: User = Depends(find_current_superuser),
 ):
     await Report.filter(id=report_id).update(**report.dict(exclude_unset=True))
     return await Report_Pydnatic.from_queryset_single(Report.get(id=report_id))
@@ -68,10 +67,9 @@ async def update_report(
 # delete a report
 @router.delete("/", response_model=Report_Pydnatic)
 async def delete_report(
-        report_id: int,
-        current_superuser: User = Depends(find_current_superuser)
+    report_id: int, current_superuser: User = Depends(find_current_superuser)
 ):
     deleted_report = await Report.filter(id=report_id).delete()
     if not deleted_report:
         raise HTTPException(status_code=404, detail=f"Report {report_id} not found")
-    return Status(message=f"Delete Report {report_id}")
+    return JSONResponse(content={"message": f"Report deleted {report_id}"})
